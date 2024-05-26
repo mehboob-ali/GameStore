86404c76cd49b60d904b725cc2e318d6221e1d00
mehboob-ali <mehboobalishaikh50@gmail.com>
2024-05-26 11:24:10 +0530

    added dropdown list for game genre in AddGame/page.jsx and a function to fetch genre data

--- frontend/src/app/AddGame/page.jsx
+++ frontend/src/app/AddGame/page.jsx
@@ -1,92 +1,115 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-const page = ({data, fetchData}) => {
+const page = ({ data, fetchData }) => {
+  const [gameName, setGameName] = useState("");
+  const [gameGenre, setGameGenre] = useState("");
+  const [gamePrice, setGamePrice] = useState("");
+  const [gameReleaseDate, setGameReleaseDate] = useState("");
+  const [genreData, setGenreData] = useState([]);
 
-    const [gameName, setGameName] = useState('');
-    const [gameGenre, setGameGenre] = useState('');
-    const [gamePrice, setGamePrice] = useState('');
-    const [gameReleaseDate, setGameReleaseDate] = useState('');
-    
-   useEffect(() => {
-    
-  
-   }, [gameName])
-   
-    
-    const handleSave=(e)=>{
-        fetch('http://localhost:5083/games',{
-            method: "POST",
-            body: JSON.stringify({
-                name: gameName,
-                genreId: parseInt(gameGenre),
-                price: parseFloat(gamePrice),
-                releaseDate: gameReleaseDate
-            }),
-            headers:{
-                "Content-Type" : "application/json" 
-            }
-        }); 
-        
-        fetchData();
-    }
+  useEffect(() => {
+    fetchGenre();
+  }, []);
 
-    const handleCancel=()=>{
-        setGameName('');
-        setGameGenre('');
-        setGamePrice('');
-        setGameReleaseDate('');
-    }
+  function fetchGenre() {
+    fetch("http://localhost:5083/genres")
+      .then((res) => res.json())
+      .then((data) => setGenreData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+    console.log("fetch genre running", genreData);
+  }
 
+  function handleGenreChange(e) {
+    console.log("game genre id", e.target.value);
+    setGameGenre(e.target.value);
+  }
+
+  const handleSave = (e) => {
+    fetch("http://localhost:5083/games", {
+      method: "POST",
+      body: JSON.stringify({
+        name: gameName,
+        genreId: parseInt(gameGenre),
+        price: parseFloat(gamePrice),
+        releaseDate: gameReleaseDate,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    fetchData();
+  };
+
+  const handleCancel = () => {
+    setGameName("");
+    setGameGenre("");
+    setGamePrice("");
+    setGameReleaseDate("");
+  };
 
   return (
-    <div className=' max-w-lg mx-auto p-4 shadow-md rounded-tr-3xl rounded-bl-3xl text-gray-600 bg-gray-200 w-1/4 border-2 h-auto border-white '>
-        <div className='pb-6'>
+    <div className=" max-w-lg mx-auto p-4 shadow-md rounded-tr-3xl rounded-bl-3xl text-gray-600 bg-gray-200 w-1/4 border-2 h-auto border-white ">
+      <div className="pb-6">
+        <span className="block mb-2 font-medium">Enter Game Name</span>
+        <input
+          type="text"
+          value={gameName}
+          onChange={(e) => setGameName(e.target.value)}
+          className="mb-3 w-full p-2 border-2 shadow-2xl border-gray-400 rounded-tl-2xl rounded-br-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
 
-            <span className='block mb-2 font-medium'>Enter Game Name</span>
-            <input type='text' 
-             value={gameName}
-             onChange={(e)=>setGameName(e.target.value)}
-             className='mb-3 w-full p-2 border-2 shadow-2xl border-gray-400 rounded-tl-2xl rounded-br-2xl focus:outline-none focus:ring-2 focus:ring-blue-500'
-             />
+        <span className="block mb-2 font-medium">Enter Genre Name</span>
 
-            <span className='block mb-2 font-medium'>Enter Genre Name</span>
-            <input type='text' 
-             value={gameGenre}
-             onChange={(e)=>setGameGenre(e.target.value)}
-             className='mb-3 w-full p-2 border-2 shadow-2xl border-gray-400 rounded-tl-2xl rounded-br-2xl focus:outline-none focus:ring-2 focus:ring-blue-500'
-             />
+        <select
+          onChange={(e) => handleGenreChange(e)}
+          className=" w-full mb-3 p-3 border-2 shadow-2xl border-gray-400 rounded-tl-2xl rounded-br-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 "
+        >
+          {genreData.map((genre, index) => (
+            <option
+              className=" items-center flex justify-center text-gray-800"
+              key={index}
+              value={genre.id}
+            >
+              {genre.name}
+            </option>
+          ))}
+        </select>
 
-            <span className='block mb-2 font-medium'>Enter Game Price</span>
-            <input type='text'
-             value={gamePrice}
-             onChange={(e)=>setGamePrice(e.target.value)}
-             className='mb-3 w-full p-2 border-2 shadow-2xl border-gray-400 focus:outline-none rounded-tl-2xl rounded-br-2xl focus:ring-2 focus:ring-blue-500'
-             />
+        <span className="block mb-2 font-medium">Enter Game Price</span>
+        <input
+          type="text"
+          value={gamePrice}
+          onChange={(e) => setGamePrice(e.target.value)}
+          className="mb-3 w-full p-2 border-2 shadow-2xl border-gray-400 focus:outline-none rounded-tl-2xl rounded-br-2xl focus:ring-2 focus:ring-blue-500"
+        />
 
-            <span className='block mb-2 font-medium'>Enter Game Release Date</span>
-            <input type='date' 
-             value={gameReleaseDate}
-             onChange={(e)=>setGameReleaseDate(e.target.value)}
-             className='w-full p-2 border-2 shadow-2xl border-gray-400 rounded-tl-2xl rounded-br-2xl focus:outline-none focus:ring-2 focus:ring-blue-500'
-            />
-        </div>
-        
-        <div className=' flex items-center justify-center gap-4'>
-            <button
-             className= ' border-2 bg-gray-500 hover:bg-gray-700 focus:ring focus:ring-gray-100 border-gray-300 p-2 px-4 text-gray-100 rounded-lg'
-             onClick={handleCancel}>
-                Cancel
-            </button>
-            
-            <button 
-             className='border-2 bg-blue-500 hover:bg-blue-700 focus:ring focus:ring-blue-300 p-2 px-4 text-gray-100 rounded-lg '
-             onClick={handleSave} >
-                Save
-            </button>
-        </div>
+        <span className="block mb-2 font-medium">Enter Game Release Date</span>
+        <input
+          type="date"
+          value={gameReleaseDate}
+          onChange={(e) => setGameReleaseDate(e.target.value)}
+          className="w-full p-2 border-2 shadow-2xl border-gray-400 rounded-tl-2xl rounded-br-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
 
+      <div className=" flex items-center justify-center gap-4">
+        <button
+          className=" border-2 bg-gray-500 hover:bg-gray-700 focus:ring focus:ring-gray-100 border-gray-300 p-2 px-4 text-gray-100 rounded-lg"
+          onClick={handleCancel}
+        >
+          Cancel
+        </button>
+
+        <button
+          className="border-2 bg-blue-500 hover:bg-blue-700 focus:ring focus:ring-blue-300 p-2 px-4 text-gray-100 rounded-lg "
+          onClick={handleSave}
+        >
+          Save
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,31 +7,78 @@ export default function Home() {
 
   useEffect(() => {
     fetchData();
-  }, [data]);
+
+  }, []);
 
   function fetchData() {
     fetch('http://localhost:5083/games')
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch((error) => console.error('Error fetching data:', error));
+      console.log("fetch running")
   }
 
+  
   const handleAddGame=()=>{
 
 
   }
 
+  function handleDelete(id){
+    fetch(`http://localhost:5083/games/${id}`,
+      {
+        method:"DELETE",
+        headers:{
+          "Content-Type" : "application/json" 
+        }
+      }
+    );
+    fetchData();
+console.log("delete running")
+  }
+
+   
+  
+
   return (
     <main className=" flex min-h-screen flex-col items-center justify-between p-8">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div>
-          <ul className="text-lg text-red-500">
+      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex bg-black">
+        <div className=" p-8">
+
+          <table className="text-lg table-auto  text-gray-300 border-gray-500 border-2 ">
+            <thead className=" text-xl font-bold ">
+              <tr className="">
+                <th className="p-2">Id</th>
+                <th>Name</th>
+                <th>Genre</th>
+                <th>Price</th>
+                <th>Release Date</th>
+              </tr>
+            </thead>
+            <tbody>
             {data.map((game, index) => (
-              <li key={index} value={game.id}>
-                {game.name}
-              </li>
+
+                <tr 
+                  className=" even:bg-gray-800 text-center"                
+                  key={index} value={game.td} >
+
+                  <td className=" p-6 ">{game.id}</td>     
+                  <td className=" p-6 ">{game.name}</td>
+                  <td className=" p-6">{game.genre}</td>
+                  <td className=" p-6 ">${game.price}</td>
+                  <td className=" p-6 ">{game.releaseDate}</td>
+                  <td><button className="p-2 mx-2 bg-blue-500 rounded-md">Edit</button></td>
+                  <td>
+                    <button className="p-2 bg-red-500 rounded-md"
+                      onClick={()=>handleDelete(game.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+                
             ))}
-          </ul>
+            </tbody>
+          </table>
           <div className="p-8">
             <button className="bg-blue-600 text-lg text-white p-2 rounded-lg"
               onClick={handleAddGame()}>Add Game</button>
